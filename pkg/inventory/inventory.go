d0a221a3ea960c6696577b01c9e7cc8d520358b6
Michael Gruener <michael.gruener@chaosmoon.net>
2019-08-03 19:43:59 +0200

    Add inventory stub

---
+++ pkg/inventory/inventory.go
@@ -0,0 +1,61 @@
+// Copyright © 2019 Michael Gruener
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package inventory
+
+//
+//import (
+//	"os"
+//
+//	"github.com/mgruener/kusible/pkg/groups"
+//
+//	"github.com/mgruener/kusible/pkg/values"
+//)
+//
+//func NewInventory(path string, ejson values.EjsonSettings) (*inventory, error) {
+//	stat, err := os.Stat(path)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	var data map[interface{}]interface{}
+//	if stat.Mode().IsRegular() {
+//		// the path provided is a file, treat it as a single value
+//		// file, thus loading it with ejson and spruc operator support
+//		inventoryFile := values.NewValueFile(path, false, ejson)
+//		data, err = inventoryFile.LoadMap()
+//		if err != nil {
+//			return nil, err
+//		}
+//	} else {
+//		// The path provided is a directory, treat it as a values
+//		// directory. As the valuesDirectory type requires a list
+//		// of groups to determine which files to process, first
+//		// get a list of all groups in the given directory
+//		groups, err := groups.Groups(path, ".*", []string{})
+//		if err != nil {
+//			return nil, err
+//		}
+//		inventoryDir := values.NewValuesDirectory(path, groups, false, ejson)
+//		data, err = inventoryDir.LoadMap()
+//		if err != nil {
+//			return nil, err
+//		}
+//	}
+//
+//	// TODO: revisit http://ghodss.com/2014/the-right-way-to-handle-yaml-in-golang/
+//	// and actually load the inventory via proper deserialization
+//
+//	return nil, nil
+//}
