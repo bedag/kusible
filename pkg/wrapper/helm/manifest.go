8e47ac22fd222ceaad8bd52eff632cb9131bd772
Michael Gruener <michael.gruener@chaosmoon.net>
2021-02-05 12:24:21 +0100

    Refactor the helm wrapper package
    The refactor ensures the structure is more in line with how
    helm itself structures its code.

---
+++ pkg/wrapper/helm/manifest.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2021 Michael Gruener & The Helm Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/* Lots of code straight from github.com/helm/helm and adapted to be used here */
+
+package helm
+
+import (
+	"helm.sh/helm/v3/pkg/releaseutil"
+)
+
+func SplitSortManifest(bigManifest string) ([]string, error) {
+	input := map[string]string{
+		"kusible": bigManifest,
+	}
+
+	_, manifests, err := releaseutil.SortManifests(input, nil, releaseutil.InstallOrder)
+	if err != nil {
+		return nil, err
+	}
+
+	result := []string{}
+	for _, manifest := range manifests {
+		result = append(result, manifest.Content)
+	}
+
+	return result, nil
+}
--- pkg/wrapper/helm/template.go
+++ pkg/wrapper/helm/template.go
@@ -20,17 +20,11 @@ package helm
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/bedag/kusible/pkg/playbook/config"
-	"github.com/pkg/errors"
 	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart/loader"
 	helmcli "helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/downloader"
-	"helm.sh/helm/v3/pkg/getter"
-	"helm.sh/helm/v3/pkg/releaseutil"
 )
 
 // TemplatePlay renders all charts contained in a given play to a string containing
@@ -62,7 +56,7 @@ func TemplatePlay(play *config.Play, settings *helmcli.EnvSettings) (string, err
 		name := chart.Chart
 		values := chart.Values
 
-		manifests, err := Template(name, values, client, settings)
+		manifests, err := template(chart.Name, name, values, client, settings)
 		if err != nil {
 			return result, err
 		}
@@ -73,59 +67,9 @@ func TemplatePlay(play *config.Play, settings *helmcli.EnvSettings) (string, err
 
 // Template renders a given chart + values to a string containing
 // kubernetes manifests
-func Template(chart string, values map[string]interface{}, client *action.Install, settings *helmcli.EnvSettings) (string, error) {
-
-	cp, err := client.ChartPathOptions.LocateChart(chart, settings)
-	if err != nil {
-		return "", err
-	}
-
-	p := getter.All(settings)
-
-	chartRequested, err := loader.Load(cp)
-	if err != nil {
-		return "", err
-	}
-
-	if chartRequested.Metadata.Type != "" && chartRequested.Metadata.Type != "application" {
-		return "", fmt.Errorf("%s charts are not installable", chartRequested.Metadata.Type)
-	}
-
-	// TODO: warning?
-	//if chartRequested.Metadata.Deprecated {
-	//	return "", fmt.Errorf("this chart is deprecated")
-	//}
-
-	if req := chartRequested.Metadata.Dependencies; req != nil {
-		// If CheckDependencies returns an error, we have unfulfilled dependencies.
-		// As of Helm 2.4.0, this is treated as a stopping condition:
-		// https://github.com/helm/helm/issues/2209
-		if err := action.CheckDependencies(chartRequested, req); err != nil {
-			if client.DependencyUpdate {
-				man := &downloader.Manager{
-					Out:              os.Stdout,
-					ChartPath:        cp,
-					Keyring:          client.ChartPathOptions.Keyring,
-					SkipUpdate:       false,
-					Getters:          p,
-					RepositoryConfig: settings.RepositoryConfig,
-					RepositoryCache:  settings.RepositoryCache,
-					Debug:            settings.Debug,
-				}
-				if err := man.Update(); err != nil {
-					return "", err
-				}
-				// Reload the chart with the updated Chart.lock file.
-				if chartRequested, err = loader.Load(cp); err != nil {
-					return "", errors.Wrap(err, "failed reloading chart after repo update")
-				}
-			} else {
-				return "", err
-			}
-		}
-	}
-
-	rel, err := client.Run(chartRequested, values)
+func template(release string, chart string, values map[string]interface{}, client *action.Install, settings *helmcli.EnvSettings) (string, error) {
+	args := []string{release, chart}
+	rel, err := runInstall(args, values, client, settings)
 
 	if err != nil && !settings.Debug {
 		if rel != nil {
@@ -135,22 +79,4 @@ func Template(chart string, values map[string]interface{}, client *action.Instal
 	}
 
 	return rel.Manifest, nil
-}
-
-func SplitSortManifest(bigManifest string) ([]string, error) {
-	input := map[string]string{
-		"kusible": bigManifest,
-	}
-
-	_, manifests, err := releaseutil.SortManifests(input, nil, releaseutil.InstallOrder)
-	if err != nil {
-		return nil, err
-	}
-
-	result := []string{}
-	for _, manifest := range manifests {
-		result = append(result, manifest.Content)
-	}
-
-	return result, nil
 }
