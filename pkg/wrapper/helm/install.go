1e363172c7af2094031852b5e596af19ab2af915
Oliver Bähler <oliverbaehler@hotmail.com>
2021-02-09 22:20:31 +0100

    feat: Publish Kusible Docker Images
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- pkg/wrapper/helm/install.go
+++ pkg/wrapper/helm/install.go
@@ -92,9 +92,6 @@ func (h *Helm) runInstall(args []string, vals map[string]interface{}, client *ac
 	}
 
 	if req := chartRequested.Metadata.Dependencies; req != nil {
-		// If CheckDependencies returns an error, we have unfulfilled dependencies.
-		// As of Helm 2.4.0, this is treated as a stopping condition:
-		// https://github.com/helm/helm/issues/2209
 		if err := action.CheckDependencies(chartRequested, req); err != nil {
 			if client.DependencyUpdate {
 				man := &downloader.Manager{
